82bd3eb7e3641fb77c0879fafaf238f2c6160fee
Chidera <chideraobi.d@gmail.com>
2024-11-21 15:01:16 +0000

    Initial commit

---
+++ products.js
@@ -0,0 +1,103 @@
+let lessons=[
+    {
+        id: 1001,
+        title: "Mastering Algebra: Foundations and Beyond",
+        subject:"Maths",
+         price: 100,
+        image: "https://images.unsplash.com/photo-1509869175650-a1d97972541a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        availableInventory: 10,
+        location:"jos",
+        rating: 2
+    },
+    {
+        id: 1002,
+        title: "Exploring the Basics of Chemistry",
+        subject:"Sciences",
+        price: 350,
+        image: "https://images.unsplash.com/photo-1621361365424-06f0e1eb5c49?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        availableInventory: 7,
+        location:"kaduna",
+        rating: 5
+    },
+    {
+    id: 1003,
+    title: "Introduction to World History: Ancient Civilisations",
+    subject:"art",
+    price: 50,
+    image: "https://images.unsplash.com/photo-1543109740-4bdb38fda756?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    availableInventory: 10,
+    location:"jos",
+    rating: 3
+},
+{
+    id: 1004,
+    title: "Creative Writing Workshop: Crafting Stories",
+    subject:"art",
+   price: 70,
+    image: "https://images.unsplash.com/photo-1621361365424-06f0e1eb5c49?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    availableInventory: 3,
+    location:"jos",
+    rating: 1
+},
+{
+    id: 1005,
+    title: "Foundations of Physics: Understanding Motion and Force",
+    subject:"maths",
+    price: 400,
+    image: "https://images.unsplash.com/photo-1543109740-4bdb38fda756?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    availableInventory: 10,
+    location:"kaduna",
+    rating: 2
+},
+{
+    id: 1006,
+    title: "Introduction to Programming with JavaScript",
+     subject:"IT",
+    price: 350,
+    image: "https://images.unsplash.com/photo-1621361365424-06f0e1eb5c49?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    availableInventory: 3,
+    location:"jos",
+    rating: 4
+},
+{
+id: 1007,
+title: "Visual Arts: Exploring Colour and Composition",
+subject:"art",
+price: 50,
+image: "https://images.unsplash.com/photo-1543109740-4bdb38fda756?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+availableInventory: 10,
+location:"kaduna",
+rating: 3
+},
+{
+id: 1008,
+title: "Fundamentals of Economics: Supply and Demand",
+subject:"Sciences",
+price: 350,
+image: "https://images.unsplash.com/photo-1621361365424-06f0e1eb5c49?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+availableInventory: 3,
+location:"jos",
+rating: 1
+},
+{
+    id: 1009,
+    title: "Digital Design Essentials: From Idea to Execution",
+   subject:"IT",
+    price: 50,
+    image: "https://images.unsplash.com/photo-1543109740-4bdb38fda756?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    availableInventory: 0,
+    location:"jos",
+    rating: 2
+},
+{
+    id: 1010,
+    title: "Geography Essentials: Climate and Ecosystems",
+    subject:"Sciences",
+    price: 350,
+    image: "https://images.unsplash.com/photo-1621361365424-06f0e1eb5c49?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    availableInventory: 0,
+    location:"kaduna",
+    rating: 5
+},
+
+]
